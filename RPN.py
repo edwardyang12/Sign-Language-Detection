4eda554bd18f1a05216af3694c9df54c2524b885
unknown <edwardyang016@gmail.com>
2020-09-25 21:34:36 -0700

    working on roi

--- RPN.py
+++ RPN.py
@@ -1,6 +1,10 @@
 from tensorflow.keras import datasets, layers, losses, optimizers, metrics, Input, Model
 import tensorflow as tf
-from feature_extraction import VGG
+from feature_extraction import prepare, VGG
+import roi_helpers
+import config
+from tensorflow.keras import backend as K
+import cv2
 
 class RPN(tf.keras.Model):
     def __init__(self, num_anchors):
@@ -15,9 +19,21 @@ class RPN(tf.keras.Model):
         regression = self.x_regr(x)
         return [classes, regression, features]
 
+# regions should be a set of regions
+def view_region(image,regions):
+    row,col = image.shape[0:2]
+    for region in regions:
+        cv2.rectangle(image, (row*region[0], col*region[1]), (row*region[2], col*region[3]), (0, 0, 255))
+    cv2.imshow('img', image)
+    cv2.waitKey(0)
+
+
 if __name__ == '__main__':
-    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-    train_images, test_images = train_images / 255.0, test_images / 255.0
+    C = config.Config()
+    arg = '2frame0.jpg'
+    test_image =  prepare(arg)
+    # (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+    # train_images, test_images = train_images / 255.0, test_images / 255.0
     input_shape_img = (None,None,3)
 
     img_input = Input(shape=input_shape_img)
@@ -30,3 +46,11 @@ if __name__ == '__main__':
 
     model = Model(inputs = img_input, outputs= rpn)
     model.compile(optimizer='sgd', loss='mse')
+
+    test = test_image.reshape(1,160,160,3)
+
+    values = model.predict(test)
+
+    R = roi_helpers.rpn_to_roi(values[0], values[1], C, K.image_data_format(), use_regr=True, overlap_thresh=0.7, max_boxes=300)
+    print(R)
+    #view_region(test_image,R)
--- roi_helpers.py
+++ roi_helpers.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pdb
 import math
-from . import data_generators
+# from . import data_generators
 import copy
 import time
 
@@ -233,16 +233,16 @@ def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=
 
     assert rpn_layer.shape[0] == 1
 
-    if dim_ordering == 'th':
+    if dim_ordering == 'channels_first': #th
         (rows,cols) = rpn_layer.shape[2:]
 
-    elif dim_ordering == 'tf':
+    elif dim_ordering == 'channels_last': #tf
         (rows, cols) = rpn_layer.shape[1:3]
 
     curr_layer = 0
-    if dim_ordering == 'tf':
+    if dim_ordering == 'channels_last':
         A = np.zeros((4, rpn_layer.shape[1], rpn_layer.shape[2], rpn_layer.shape[3]))
-    elif dim_ordering == 'th':
+    elif dim_ordering == 'channels_first':
         A = np.zeros((4, rpn_layer.shape[2], rpn_layer.shape[3], rpn_layer.shape[1]))
 
     for anchor_size in anchor_sizes:
@@ -250,7 +250,7 @@ def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=
 
             anchor_x = (anchor_size * anchor_ratio[0])/C.rpn_stride
             anchor_y = (anchor_size * anchor_ratio[1])/C.rpn_stride
-            if dim_ordering == 'th':
+            if dim_ordering == 'channels_first':
                 regr = regr_layer[0, 4 * curr_layer:4 * curr_layer + 4, :, :]
             else:
                 regr = regr_layer[0, :, :, 4 * curr_layer:4 * curr_layer + 4]
